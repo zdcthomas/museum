365e8bba0282225983684f2b34deaed50d28985e
zdcthomas <zdcthomas@yahoo.com>
2018-03-28 09:33:21 -0600

    TESTS museums can have names

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,2 +1,2 @@
 class Museum
-end
+end 
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,7 +5,21 @@ require "Minitest/pride"
 
 class MuseumTest < MiniTest::Test
   def test_museum_exists
-    museum = Museum.new
+    museum = Museum.new("DMA")
     assert_instance_of Museum, museum
+  end
+
+  def test_museum_has_name
+    museum = Museum.new("DMA")
+    assert_equal "DMA", museum.name
+    museum = Museum.new("San Fransisco Museum of Art")
+    assert_equal "San Fransisco Museum of Art", museum.name
+    museum = Museum.new("Denver Art Museum")
+    assert_equal "Denver Art Museum", museum.name
+    museum = Museum.new("La Louvre")
+    assert_equal "La Louvre", museum.name
+    name = (0...8).map { (65 + rand(26)).chr }.join
+    museum = Museum.new(name)
+    assert_equal name, museum.name
   end
 end
