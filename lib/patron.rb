ff31760720b11fd33fbfddd0a23f900b35b69dac
zdcthomas <zdcthomas@yahoo.com>
2018-03-28 09:17:18 -0600

    TESTS patrons can have names

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,4 +1,4 @@
 class Patron
   def initialize
-  end 
+  end
 end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -7,8 +7,17 @@ class PatronTest < MiniTest::Test
   def setup
   end
 
-  def test_it_exists
+  def test_it_existsg
     patron = Patron.new
     assert_instance_of Patron, patron
+  end
+
+  def test_patron_can_have_name
+    patron = Patron.new("Jim")
+    assert_equal "Jim", patron.name
+    patron = Patron.new("Harold")
+    assert_equal "Harold", patron.name
+    patron = Patron.new("Samantha")
+    assert_equal "Samantha", patron.name
   end
 end
